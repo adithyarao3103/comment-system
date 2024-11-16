35e44b8404b8bbc00f5c07e988188ae262eb7859
adithyarao3103 <adithyarao3132001@gmail.com>
2024-11-16 21:43:51 +0530

    add comm

--- api/add-comment.js
+++ api/add-comment.js
@@ -3,27 +3,28 @@ import { createClient } from '@supabase/supabase-js';
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
 export default async function handler(req, res) {
-if (req.method !== 'POST') {
-    return res.status(405).send('Method Not Allowed');
-}
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
 
-const { name, email, comment} = req.query;
+    const { name, email, comment} = req.query;
 
-if (!name || !comment) {
-    return res.status(400).json({ error: 'Missing required fields' });
-}
+    if (!name || !comment) {
+        return res.status(400).json({ error: 'Missing required fields' });
+    }
 
-email = email || '';
+    email = email || '';
 
-const date =  new Date().toISOString().split('T')[0];
+    const date =  new Date().toISOString().split('T')[0];
 
-const { data, error } = await supabase.from('comments').insert([
-    { name: name, email: email , comment: comment, date: date, verified: false }
-]);
+    const { data, error } = await supabase.from('comments').insert([
+        { name: name, email: email , comment: comment, date: date, verified: false }
+    ]);
 
-if (error) {
-    return res.status(500).json({ error: error.message });
-}
+    if (error) {
+        return res.status(500).json({ error: error.message });
+    }
 
-res.status(200).json({ message: 'Comment added', data });
+    res.status(200).json({ message: 'Comment added', data });
 }
